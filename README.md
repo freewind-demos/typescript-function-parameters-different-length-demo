e4ff5914f1ad2a389540404e8d4f2222b7bb64fc
freewind <nowindlee@gmail.com>
2019-05-21 15:39:27 +0800

    demo

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Function Parameters Different Length Demo
+===================================================
+
+如何在typescript定义一个函数，它有两种调用方式，一种只有一个参数，另一种有两个参数（且第一个参数类型与第一种不同）
+
+在这个Demo中的实现函数：
+
+```
+function visit(...params: any): void
+```
+
+中，使用了`...params: any`，虽然可用，但是实际上失去了类型安全，但是也没有找到更好的做法。
+
+https://stackoverflow.com/questions/56232992/how-to-define-param-names-and-types-for-a-function-which-accepts-1-parameter-or
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,1 +1,26 @@
-console.log('Hello');
+type VisitOptions = {
+  timeout: number
+}
+
+function visit(url: string, options: Partial<VisitOptions>): void;
+function visit(options: Partial<VisitOptions> & { url: string }): void;
+function visit(...params: any): void {
+  let finalOptions: Partial<VisitOptions> & { url: string };
+  if (params.length === 1) {
+    finalOptions = params[0];
+  } else {
+    const [url, options] = params;
+    finalOptions = {
+      url,
+      ...options
+    }
+  }
+  console.log(finalOptions);
+}
+
+visit('http://localhost', {timeout: 100});
+visit({
+  url: 'http://localhost',
+  timeout: 100
+});
+
